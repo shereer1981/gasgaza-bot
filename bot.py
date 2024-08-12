6c522f70b0c7640883b47f785ec3471917d85100
shereer1981 <sameralsmeri@gmail.com>
2024-08-12 13:48:44 +0300

    Update bot.py
    Updat

--- bot.py
+++ bot.py
@@ -1,31 +1,20 @@
-python
 import pandas as pd
 import requests
 from io import BytesIO
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 import os
-from flask import Flask
-
-# إعدادات Flask
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "Bot is running!"
 
 # رابط الجوجل شيتس
 SHEET_ID = '1--Lcu0S2dSgH81qHNKPpiF9vGeSnqZiZ'
 SHEET_URL = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=xlsx'
 
 def download_file(url):
-    """تحميل ملف Excel من جوجل شيتس"""
     response = requests.get(url)
     response.raise_for_status()
     return BytesIO(response.content)
 
 def start(update: Update, context: CallbackContext) -> None:
-    """إرسال رسالة ترحيبية عند بدء التفاعل مع البوت"""
     start_message = (
         "👋 أهلاً! أرسل رقم الهوية للبحث. \n\n"
         "📋 مثال: 123456789\n"
@@ -34,7 +23,6 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(start_message)
 
 def search(update: Update, context: CallbackContext) -> None:
-    """البحث في ملف جوجل شيتس عن رقم الهوية المرسل من المستخدم"""
     update.message.reply_text('🔄 بدء عملية البحث، يرجى الأنتظار...')
     
     query = update.message.text
@@ -53,12 +41,9 @@ def search(update: Update, context: CallbackContext) -> None:
         update.message.reply_text('⚠️ حدث خطأ أثناء محاولة الوصول إلى البيانات.')
 
 def main() -> None:
-    """إعداد البوت وتشغيله"""
-    TOKEN = os.getenv("TELEGRAM_TOKEN")
-    if not TOKEN:
-        raise ValueError("No TELEGRAM_TOKEN found in environment variables")
-    
+    TOKEN = "7124688280:AAGB4MzzUNc9aYEeeLhnDO7Q5Coa6IQZuwg"
     updater = Updater(TOKEN, use_context=True)
+
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CommandHandler("start", start))
@@ -69,4 +54,3 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-    app.run(host='0.0.0.0', port=8080)
